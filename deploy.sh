04859c076c189b3b27aa10ca91e37f5a361a62ac
Luiz Antonio Adolphs Junior <luiz.adolphs@gmail.com>
2017-12-29 16:23:18 -0200

    removing nuget source

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 ApiKey=$1
-Source=$2
 
 nuget pack ./NuScrap.nuspec -Verbosity detailed
 
-nuget push ./NuScrap.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
+nuget push ./NuScrap.*.nupkg -Verbosity detailed -ApiKey $ApiKey
